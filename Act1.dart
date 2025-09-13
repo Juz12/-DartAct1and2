b7b30863c073d070dbaab2bfab9fde41755a10b0
Juz12 <corderojustin0205@gmail.com>
2025-09-13 03:40:50 +0000

    Activity 1 and 2 : Understanding the Dart

---
+++ Act1.dart
@@ -0,0 +1,12 @@
+// void main(){
+// var greeting = "Welcome";
+// var user = 'Alice";
+// print('$greeting, $user);
+//}
+
+void main(){
+    var greeting = "Welcome";
+    dynamic user = "Alice";
+    user = 25;
+    print("$greeting, $user");
+}
